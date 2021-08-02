58dba546592fffe5dd429aea8086f0aa6f958a4c
Brianna Bias <brianna.bias@gmail.com>
2021-08-02 03:43:46 -0700

    getbyid genre test added

--- __tests__/genre.test.js
+++ __tests__/genre.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Genre from '../lib/models/Genre.js';
+import Genre from '../lib/models/Genre.js';
 
 describe('genre CRUD routes', () => {
   beforeEach(() => {
@@ -12,8 +12,15 @@ describe('genre CRUD routes', () => {
   it('creates a genre via POST', async () => {
     const genre = { name: 'New Wave', type: 'synth' };
     const res = await request(app).post('/api/v1/genres').send(genre);
-    
+
     expect(res.body).toEqual({ id: '1', ...genre });
+  });
+
+  it('gets a genre by id', async () => {
+    const genre = await Genre.insert({ name: 'New Wave', type: 'synth' });
+    const res = await request(app).get(`/api/v1/genres/${genre.id}`);
+
+    expect(res.body).toEqual(genre);
   });
 });
 
